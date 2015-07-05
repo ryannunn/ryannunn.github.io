8aa39b1dc084a0202cbdc818193a032cb4e35582
ryannunn <rnunn25@gmail.com>
2015-07-05 14:16:23 -0700

    changed to index added jquery file
    above

---
+++ practice.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $(".box p").fadeIn(1750);
+    
+    $('img').fadeIn(1500);
+    
+    $button = $('button');
+	$button.hide();
+	var timeOut = setTimeout(function() {
+		$button.fadeIn('slow');
+	}, 6500);
+    
+    $('.jumbotron .container h2').fadeIn(3000)
+	
+	$('button').click(function(){
+		$(this).addClass('btn-primary disabled');    
+	    $('p').hover(function(){
+	            $(this).fadeOut('slow');
+	    });
+	    $('li').hover(function(){
+	            $(this).fadeOut('slow');
+	    });
+	    $('h2').hover(function(){
+	            $(this).fadeOut('slow');
+	    });
+	    $('.disabled').hover(function(){
+		    	$(this).fadeOut('slow');
+	    });   
+		$('img').hover(function(){
+				$(this).fadeOut('slow');
+		});
+   });
+});
